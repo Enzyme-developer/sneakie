708070fd0ab5f4168fc2afc3b5da083f9f5d9e55
Ayoe <dannyhopkins20003@gmail.com>
2022-08-16 10:51:49 +0100

    implemented filters

--- firebase.ts
+++ firebase.ts
@@ -3,8 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from  'firebase/auth'
 import { getFirestore } from  'firebase/firestore'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: "sneakie-mart.firebaseapp.com",
